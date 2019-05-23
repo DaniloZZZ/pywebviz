09d76e86e819bbc3a2465eb8fb149d02182e12f1
Danil Lykov <lkv97dn@gmail.com>
2019-05-23 23:17:55 +0300

    fixed crashing if no matplotlib or bokeh installed

--- python/webvis/interface.py
+++ python/webvis/interface.py
@@ -1,18 +1,32 @@
 import json
 #from . import helpers as h
-import matplotlib
-import mpld3
-import bokeh
+try:
+    import matplotlib
+    import mpld3
+    import bokeh
+except Exception as e:
+    print(e)
+
+def is_mpl(val):
+    try:
+        return isinstance(val, matplotlib.figure.Figure)
+    except Exception as e:
+        return False
+def is_bokeh(val):
+    try:
+        return isinstance(val,bokeh.model.Model) or isinstance(val,bokeh.document.document.Document)
+    except Exception as e:
+        return False
 
 def get_var(val, params):
     """
     Val: some value from user
     params: dict of params from frontend
     """
-    if isinstance(val,bokeh.model.Model) or isinstance(val,bokeh.document.document.Document):
+    if is_bokeh(val):
         ret = bokeh.embed.file_html(val, bokeh.resources.Resources('cdn'))
         type_ = 'mpl'
-    elif type(val)==matplotlib.figure.Figure:
+    elif is_mpl(val):
         ret = mpld3.fig_to_html(val)
         type_ = 'mpl'
     else:
