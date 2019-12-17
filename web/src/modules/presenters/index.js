b2d2ccd558a4c3f18bbb7ef48ae5a92f6c26278f
Danil Lykov <lkv97dn@gmail.com>
2019-12-17 01:01:50 -0600

    add modularity for presenters

---
+++ web/src/modules/presenters/index.js
@@ -0,0 +1,7 @@
+export {default as LineGraph} from './lineGraph_recharts.coffee'
+export {default as Image} from './image.coffee'
+export {default as MplD3} from './mpld3.coffee'
+export function Raw({data}){return data}
+
+// installed modules
+export {default as TextModule} from './custom/textModule.coffee'
