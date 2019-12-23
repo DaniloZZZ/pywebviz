563c3ae872c181ade1848455dd8f6244106c27a0
Danil Lykov <lkv97dn@gmail.com>
2019-12-23 16:18:16 -0600

    abstracted dir structure creation and root import

--- mods/webvis_mods/imports.py
+++ mods/webvis_mods/imports.py
@@ -1,11 +1,30 @@
 from pathlib import Path
-
-def import_str_python(modname):
-    return f"from .{modname} import {modname}"
-def import_str_js(modname):
-    return f"export {{default as {modname}}} from './{modname}'"
+from . import utils
 
 def generate_index(import_str, mod_dir):
     mod_dir = Path(mod_dir)
     mods = [x.name for x in mod_dir.iterdir() if x.is_dir()]
-    return '\n'.join(map(import_str, mods))
+    x = '\n'.join(map(import_str, mods))
+    return x
+
+## Python
+
+def import_str_python(modname):
+    return f"from .{modname} import {modname}"
+
+def root_import_py(src_file, moddir):
+    modname = moddir.name
+    utils.write_to(f"from .{src_file.stem} import {modname} ",
+             moddir/'__init__.py')
+
+## JS
+
+def import_str_js(modname):
+    return f"export {{default as {modname}}} from './{modname}'"
+
+def root_import_js(src_file, moddir):
+    modname = moddir.name
+    x = f"import {{default as {modname}}} from './{src_file.name}';\
+            export default {modname}"
+    utils.write_to(x, moddir/'index.js')
+
--- mods/webvis_mods/main.py
+++ mods/webvis_mods/main.py
@@ -5,9 +5,11 @@ from pathlib import Path
 from loguru import logger as log
 
 from imports import (
-    import_str_python
+    generate_index
+    , import_str_python
+    , root_import_py
     , import_str_js
-    , generate_index
+    , root_import_js
 )
 
 from .utils import run_cmd, copy, write_to
@@ -24,47 +26,48 @@ build_dir = vis_dir / 'front_build'
 python_mods_dir = vis_dir / 'modules'
 python_user_mods = python_mods_dir / 'installed'
 
+def _dir_struct(src, usr_mods, modname):
+    """
+    Put `src` into `usr_mods/modname` directory.
+    Create if does not exist
+
+    Returns
+    -------
+    moddir: pathlib.Path
+        the root of files copied from `src` directory
+    """
+    moddir = usr_mods / modname
+    makedirs(moddir, exist_ok=True)
+    copy(src, moddir)
+    return moddir
+
 def init_mod(name, path='~/webvis_modules/'):
     path = Path(path)
     mod_path = path / name
     makedirs(mod_path / 'back', exist_ok=True)
     makedirs(mod_path / 'front', exist_ok=True)
 
-
 def install_mod(back_src, front_src, modname):
-    back_src = Path(back_src)
-    front_src = Path(front_src)
-    makedirs(web_user_mods, exist_ok=True)
-    makedirs(python_user_mods, exist_ok=True)
-    log.debug("Web modules path: {}", web_src)
+    back_src, front_src = Path(back_src), Path(front_src)
+    #Back
+    back_moddir = _dir_struct(back_src, python_user_mods, modname)
 
-
-    back_moddir = python_user_mods / modname
-    front_moddir = web_user_mods / modname
-    # Install module
-    makedirs(back_moddir, exist_ok=True)
-    makedirs(front_moddir, exist_ok=True)
-
-    ## Copy files
-    copy(back_src, back_moddir)
-    copy(front_src, front_moddir)
-
-    ## Set up import of module
     if back_src.is_file():
-        write_to(f"from .{back_src.stem} import {modname} ",
-                 back_moddir/'__init__.py')
+        root_import_py(back_src, back_moddir)
 
-    # Update index imports
     index_py = generate_index(import_str_python, python_user_mods)
     write_to(index_py, python_user_mods/'__init__.py')
 
-    if front_src.is_file():
-        imp = f"import {{default as {modname}}} from './{front_src.name}'; export default {modname}"
+    # Front
+    log.debug("Web modules path: {}", web_src)
+    front_moddir = _dir_struct(front_src, web_user_mods, modname)
 
-        write_to(imp, front_moddir/'index.js')
+    if front_src.is_file():
+        root_import_js(front_src, front_moddir)
     index_js = generate_index(import_str_js, web_user_mods)
     write_to(index_js, web_user_mods/'index.js')
 
     ## Build the front and copy dist
+    print(f"Building the app from {web_src}...")
     run_cmd([manager_path/'build.sh', web_src])
     run_cmd(['rsync', '-r', web_src/'dist', build_dir])
