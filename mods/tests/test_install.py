f5f75fcd02b404caeb386f632ff54cc876ff28ae
Danil Lykov <lkv97dn@gmail.com>
2019-12-24 22:44:33 -0600

    testing modules install/uninstall

--- mods/tests/test_install.py
+++ mods/tests/test_install.py
@@ -1,4 +1,7 @@
+from importlib import reload
+import webvis
 import webvis_mods
+
 from pathlib import Path
 
 mocks = Path(__file__).parent / 'mocks'
@@ -6,3 +9,16 @@ mocks = Path(__file__).parent / 'mocks'
 pyfile, webfle = mocks/'module.py', mocks/'blah.coffee'
 
 webvis_mods.install_mod(pyfile, webfle, 'Test')
+from webvis.modules import Test
+t = Test()
+t = None
+webvis_mods.uninstall_mod('Test')
+webvis = reload(webvis)
+
+pyfile, webfle = mocks/'BirModule'/'back', mocks/'BirModule'/'front'
+
+webvis_mods.install_mod(pyfile, webfle, 'BirModule')
+from webvis.modules import BirModule
+m = BirModule(count=5)
+m.serial()
+webvis_mods.uninstall_mod('BirModule')
