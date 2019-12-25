36e99898e2950badbb7cac34aed7ee8e23d561d9
Danil Lykov <lkv97dn@gmail.com>
2019-12-24 23:00:44 -0600

    testing modules install/uninstall (reload not working)

--- mods/tests/test_install.py
+++ mods/tests/test_install.py
@@ -1,4 +1,5 @@
 from importlib import reload
+import webvis.modules as modules
 import webvis
 import webvis_mods
 
@@ -9,16 +10,20 @@ mocks = Path(__file__).parent / 'mocks'
 pyfile, webfle = mocks/'module.py', mocks/'blah.coffee'
 
 webvis_mods.install_mod(pyfile, webfle, 'Test')
-from webvis.modules import Test
-t = Test()
+modules = reload(modules)
+reload(webvis)
+
+t = modules.Test()
 t = None
 webvis_mods.uninstall_mod('Test')
-webvis = reload(webvis)
 
 pyfile, webfle = mocks/'BirModule'/'back', mocks/'BirModule'/'front'
 
 webvis_mods.install_mod(pyfile, webfle, 'BirModule')
-from webvis.modules import BirModule
-m = BirModule(count=5)
+modules = reload(modules)
+reload(webvis)
+
+m = modules.BirModule(count=5)
 m.serial()
+m = None
 webvis_mods.uninstall_mod('BirModule')
